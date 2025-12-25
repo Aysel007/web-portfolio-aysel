551ab6487b89f30305993439886e6d80fbe63b9b
aysel <ayselghazwan@gmail.com>
2025-12-26 00:16:11 +0700

    update konfigurasi deploy

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // Ganti link panjang tadi dengan nama repo saja, diapit garis miring /
   base: "/web-portfolio-aysel/",
 });
